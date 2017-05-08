2712e818c124012ea0a36df880c2df71588d7f07
Tanveer Momin <tanveermomin@gmail.com>
2017-05-08 10:14:57 -0500

    AKF Golf 2017

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('title', 'Welcome')
+
+@section('content')
+
+<div class="container theme-showcase" role="main">
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+        <h1>Welcome to AKF Golf 2017</h1>
+        <p>This site is to be used to check players on the of the tournament.</p>
+    </div>
+
+</div> <!-- /container -->
+
+@endsection
