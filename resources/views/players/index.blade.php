76a639de4ae88852142372ad10665eaf78bab220
Tanveer Momin <tanveermomin@gmail.com>
2017-05-25 02:47:00 -0500

    AKF Golf 2017

--- resources/views/players/index.blade.php
+++ resources/views/players/index.blade.php
@@ -68,9 +68,13 @@
                                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
                                     <button class="btn btn-primary" onclick="$(this).find('form').submit();" disabled="disabled">Checkin</button>
                                 @else
-                                    <input type="hidden" name="_method" value="POST">
-                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                    <button class="btn btn-primary" onclick="$(this).find('form').submit();">Checkin</button>
+                                    @if($player->checkin == true)
+                                        <button class="btn btn-primary" disabled="disabled">Checked-In</button>
+                                    @else
+                                        <input type="hidden" name="_method" value="POST">
+                                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                        <button class="btn btn-primary" onclick="$(this).find('form').submit();">Checkin</button>
+                                    @endif
                                 @endif
                                 </form>
                             </td>
