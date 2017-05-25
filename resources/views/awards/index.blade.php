9c0ccec1bcee6332368874568998169f4558ec24
tmomin <tanveermomin@gmail.com>
2017-05-25 14:42:31 -0500

    AKF Golf 2017

--- resources/views/awards/index.blade.php
+++ resources/views/awards/index.blade.php
@@ -76,6 +76,37 @@
                 </table>
             </div>
             {{--@include('partials.addplayer')--}}
+            <div class="row col-md-10 col-md-offset-0">
+                <form class="form-inline" method="post" action="{{ url('/players') }}">
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                    <div class="form-group">
+                        <label class="sr-only" for="firstName">First Name</label>
+                        <input type="text" class="form-control" id="firstName" name="firstName" placeholder="First Name" required>
+                    </div>
+                    <div class="form-group">
+                        <label class="sr-only" for="lastName">Last Name</label>
+                        <input type="text" class="form-control" id="lastName" name="lastName" placeholder="Last Name" required>
+                    </div>
+                    <div class="form-group">
+                        <label class="sr-only" for="companyId">Company</label>
+                        <select class="form-control" id="companyId" name="companyId" required>
+                            <option value="" disabled selected>Select Company</option>
+                            @foreach($companies as $company)
+                                <option value="{{ $company->id }}">{{ $company->companyName }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label class="sr-only" for="email">Email</label>
+                        <input type="text" class="form-control" id="email" name="email" placeholder="email">
+                    </div>
+                    <div class="form-group">
+                        <label class="sr-only" for="phone">Phone</label>
+                        <input type="text" class="form-control" id="phone" name="phone" placeholder="phone">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Add Player</button>
+                </form>
+            </div>
         </div>
     </div> <!-- /container -->
 
